a4e77df488a5313efbda72d86293c8552628a57e
kush sharma <kush.barotha@gmail.com>
2023-03-06 14:04:23 +0530

    adding

---
+++ src/controllers/v1/webApp/job.details.js
@@ -0,0 +1,4 @@
+const jobDetailsModel=require("../models/jobDetails.mode.js");
+const userDetailsModel=require("../models/user.model.js");
+const jobCategoryDetailsModel=require("../models/jobCategory.model.js");
+
